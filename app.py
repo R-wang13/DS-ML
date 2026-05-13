5f6c42e0702cceb841640c0cfd73fa86727d2ae4
R-wang13 <patjaza2000@gmail.com>
2026-05-13 14:26:59 +0700

    Update app.py

--- app.py
+++ app.py
@@ -2,8 +2,8 @@ import streamlit as st
 
 st.set_page_config(page_title="MyApp", layout="wide")
 
+st.title("### R-Wang 13 T")
 st.title("🏠 หน้าหลัก ")
-st.write("### R-Wang 13 T")
 st.write("### Boot Camp: Data Science and Machine Learning")
 st.info("7 Day Intensive Hands-on Workshop")
 st.write("##### Day 1: การจัดการข้อมูลพื้นฐานและโครงสร้างข้อมูลด้วย Python")
