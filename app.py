d8dcbcbfe6dc16c4f0dfd144f61a2d153f06d2aa
R-wang13 <patjaza2000@gmail.com>
2026-05-22 16:01:26 +0700

    Update app.py

--- app.py
+++ app.py
@@ -10,5 +10,5 @@ st.write("##### Day 1: การจัดการข้อมูลพื้น
 
 if st.button(''':rainbow["💰 ระบบคำนวณส่วนลดตามยอดซื้อ"]'''):
     st.switch_page("pages/app1_discount_calc.py")
-elif st.button("🧹ทำความสะอาดข้อมูล"):
+elif st.button(''':rainbow["🧹ทำความสะอาดข้อมูล"]'''):
     st.switch_page("pages/clean_app.py")
