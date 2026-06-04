76cac1cae84f7e4a4b3914ff02f9e5fdef6f4b6d
R-wang13 <patjaza2000@gmail.com>
2026-06-04 11:37:49 +0700

    Update app.py

--- app.py
+++ app.py
@@ -8,11 +8,13 @@ st.write("### Boot Camp: Data Science and Machine Learning")
 st.info("7 Day Intensive Hands-on Workshop")
 st.write("##### Day 1: การจัดการข้อมูลพื้นฐานและโครงสร้างข้อมูลด้วย Python")
 
-if st.button(''':rainbow[💰 ระบบคำนวณส่วนลดตามยอดซื้อ]'''):
+if st.button(''':rainbow[ระบบคำนวณส่วนลดตามยอดซื้อ]'''):
     st.switch_page("pages/app1_discount_calc.py")
-elif st.button(''':rainbow[🧹ทำความสะอาดข้อมูล]'''):
+elif st.button(''':rainbow[ทำความสะอาดข้อมูล]'''):
     st.switch_page("pages/clean_app.py")
-elif st.button(''':rainbow[♻️การแปลงข้อมูล]'''):
+elif st.button(''':rainbow[การแปลงข้อมูล]'''):
     st.switch_page("pages/transform_app.py")
-elif st.button(''':rainbow[📈การวิเคราะห์ข้อมูลเชิงสำรวจ]'''):
-    st.switch_page("pages/EDA_app.py")      
+elif st.button(''':rainbow[การวิเคราะห์ข้อมูลเชิงสำรวจ]'''):
+    st.switch_page("pages/EDA_app.py")
+elif st.button(''':rainbow[การพยากรณ์ยอดขายแบบง่าย]'''):
+    st.switch_page("pages/EDA_app.py") 
